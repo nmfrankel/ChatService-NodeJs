bddb4d46c3aeab547f6a705279a9b926a2540b33
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-02-16 23:19:30 +0200

    Server API setup

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 80
+const methodOverride = require('method-override');
+const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// parse request bodies (req.body)
+app.use(express.urlencoded({ extended: true }))
+
+// allow overriding methods in query (?_method=put)
+app.use(methodOverride('_method'));
+
+/* ROUTER:START */
+/* ROUTER:START */
+
+// static pages load from /public
+app.use(express.static('public'))
+
+// api endpoint
+app.use('/api', require('./api.js'))
+
+
+/* ROUTER:END */
+/* ROUTER:END */
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
+
+/*
+app.use(express.static('public')) // just middleware
+app.use('/static', express.static('public'))// middleware with a defined path
+
+app.use('/birds', require('./birds.js'))
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+*/
